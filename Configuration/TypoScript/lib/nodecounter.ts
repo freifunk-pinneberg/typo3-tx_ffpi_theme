6341879dc672e179a26bf152f585822b2ec5c8e2
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2016-08-15 20:43:24 +0200

    Update für nodecounter 2.0

--- Configuration/TypoScript/lib/nodecounter.ts
+++ Configuration/TypoScript/lib/nodecounter.ts
@@ -1,8 +1,18 @@
 ## -------- Node Counter ------------
+#lib.nodecounter = COA
+#lib.nodecounter {
+#    10 < plugin.tx_ffpinodecounter
+#    10.settings.nodeListFile = http://meshviewer.pinneberg.freifunk.net/data/nodelist.json
+#    10.settings.nodeListExternal = TRUE
+#}
+
+plugin.tx_ffpinodecounter {
+    settings.nodeListFile = http://meshviewer.pinneberg.freifunk.net/data/nodes.json
+    settings.nodeListExternal = TRUE
+}
+
 lib.nodecounter = COA
 lib.nodecounter {
-    10 < plugin.tx_ffpinodecounter_pi1
-    10.nodeListFile = http://meshviewer.pinneberg.freifunk.net/data/nodelist.json
-    10.templateFile = EXT:ffpi_nodecounter/pi1/templates/counter.html
-    10.nodeListExternal = TRUE
+
+    15 < tt_content.list.20.ffpinodecounter_counter
 }
