4d3f4c76ad4acf6d99109342374de1abc64924ab
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2018-12-02 16:24:19 +0100

    WIP: Composer

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version'          => '2.0.0',
     'constraints'      => array(
         'depends'   => array(
-             'typo3' => '8.7.0-9.6.99',
+             'typo3' => '8.7.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests'  => array(
