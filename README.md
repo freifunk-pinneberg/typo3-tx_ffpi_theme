61b281e062015c5a5b7349800801d2086b78b00d
Kevin Quiatkowski <k.quiatkowski@atlantismedia.de>
2022-05-13 14:36:27 +0200

    Improve metadata

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This Plugin brings the Design to https://pinneberg.freifunk.net
 * The Code is licensed under the GPLv3
 * The Roboto Fonts are from Google Fonts and licensed under the Apache License
 * The Resulting Design from this Plugin is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License (use https://pinneberg.freifunk.net as attribution)
-* The Freifunk Pinneberg Logo is not free
+* The Freifunk Pinneberg Logo is **not** free, please use your own.
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,33 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-    'title'            => 'Freifunk Pinneberg Theme',
-    'description'      => 'Theme for Freifunk Pinneberg',
-    'category'         => 'distribution',
-    'author'           => 'Kevin Quiatkowski',
-    'author_email'     => 'kevin@pinneberg.freifunk.net',
-    'author_company'   => 'Freifunk Pinneberg',
-    'shy'              => '',
-    'priority'         => '',
-    'module'           => '',
-    'state'            => 'stable',
-    'internal'         => '',
-    'uploadfolder'     => '0',
-    'createDirs'       => '',
-    'modify_tables'    => '',
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Freifunk Pinneberg Theme',
+    'description' => 'Theme for Freifunk Pinneberg',
+    'category' => 'distribution',
+    'author' => 'Kevin Quiatkowski',
+    'author_email' => 'kevin@pinneberg.freifunk.net',
+    'author_company' => 'Freifunk Pinneberg',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'lockType'         => '',
-    'version'          => '2.0.0',
-    'constraints'      => array(
-        'depends'   => array(
-             'typo3' => '8.7.0-9.5.99',
-        ),
-        'conflicts' => array(),
-        'suggests'  => array(
+    'lockType' => '',
+    'version' => '2.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [
             'ffpi_nodecounter' => '',
             'ffpi_node_updates' => '',
             'rte_ckeditor_image' => '',
-            'tx_seo' => ''
-        ),
-    ),
-);
+            'tx_seo' => '',
+        ],
+    ],
+];
