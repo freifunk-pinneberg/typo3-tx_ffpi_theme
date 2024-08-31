1b41593aff6137ad80e88ab303e9a60dad39b56f
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2024-08-31 18:55:33 +0200

    Make headlines adjustable

--- Configuration/TCA/Overrides/ContentElements/PictureTeaser.php
+++ Configuration/TCA/Overrides/ContentElements/PictureTeaser.php
@@ -8,6 +8,7 @@ return [
             --palette--;;language,
             --palette--;' . $llFrontend . 'palette.general;general,
             header,
+            header_layout,
             bodytext,
             media,
             pi_flexform,
