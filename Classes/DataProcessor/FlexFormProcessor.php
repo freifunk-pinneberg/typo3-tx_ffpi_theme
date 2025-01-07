6c3976907a638b6f9a833013a6c90f813d4cbaca
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2025-01-07 19:54:35 +0100

    Make phpstan happy

--- Classes/DataProcessor/FlexFormProcessor.php
+++ Classes/DataProcessor/FlexFormProcessor.php
@@ -48,9 +48,9 @@ class FlexFormProcessor implements DataProcessorInterface
      * @param array $contentObjectConfiguration The configuration of Content Object
      * @param array $processorConfiguration The configuration of this processor
      * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
-     * @return array the processed data as key/value store
+     * @return array<mixed> the processed data as key/value store
      */
-    public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData)
+    public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData): array
     {
         // The field name to process
         $fieldName = $cObj->stdWrapValue('fieldName', $processorConfiguration);
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,30 +13,31 @@ $llFrontendDb = 'LLL:EXT:frontend/Resources/Private/Language/Database.xlf:';
 // Add Content Elements from Subfolder
 $files = glob(__DIR__ . '/ContentElements/*.php');
 
-foreach ($files as $file) {
-    /** @var array $contentElement */
-    $contentElement = include $file;
-    $contentName = strtolower($extensionKey . '_' . $contentElement['name']);
+if($files) {
+    foreach ($files as $file) {
+        /** @var array<mixed> $contentElement */
+        $contentElement = include $file;
+        $contentName = strtolower($extensionKey . '_' . $contentElement['name']);
 
-    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-        [
-            $ll . $contentElement['name'] . '.wizard.title',
-            $contentName,
-            $contentElement['icon'] ?: 'EXT:' . $extensionPath . '/ext_icon.svg'
-        ],
-        TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-        $extensionPath
-    );
-    $GLOBALS['TCA']['tt_content']['types'][$contentName] = [
-        'showitem' => $contentElement['showitem'],
-        'columnsOverrides' => $contentElement['columnsOverrides']
-    ];
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+            [
+                $ll . $contentElement['name'] . '.wizard.title',
+                $contentName,
+                $contentElement['icon'] ?: 'EXT:' . $extensionPath . '/ext_icon.svg'
+            ],
+            TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+            $extensionPath
+        );
+        $GLOBALS['TCA']['tt_content']['types'][$contentName] = [
+            'showitem' => $contentElement['showitem'],
+            'columnsOverrides' => $contentElement['columnsOverrides']
+        ];
 
-    if ($contentElement['flexform']) {
-        $GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds'][',' . $contentName] =
-            'FILE:EXT:' . $extensionPath . '/Configuration/FlexForms/' . ucfirst($contentElement['name']) . '.xml';
+        if ($contentElement['flexform']) {
+            $GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds'][',' . $contentName] =
+                'FILE:EXT:' . $extensionPath . '/Configuration/FlexForms/' . ucfirst($contentElement['name']) . '.xml';
+        }
     }
 }
-
 $GLOBALS['TCA']['tt_content']['columns']['hidden']['config']['l10n_mode'] = 'mergeIfNotBlank';
 $GLOBALS['TCA']['tt_content']['columns']['hidden']['config']['l10n_display'] = 'defaultAsReadonly';
