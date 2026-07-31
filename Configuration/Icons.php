b41fbf86f70c8ff0505f6e8046c4242038606b88
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2026-07-31 19:01:08 +0200

    V13 fixes

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -6,38 +6,38 @@ use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 return [
     'ffpitheme_stage' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi-theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_video_teaser' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi-theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_picture_teaser' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi-theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_teaser_cards' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi_theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_wide_teaser_cards' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi_theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_grid_cards' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi_theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_news_menu' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi_theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_subpage_menu' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi_theme/Resources/Public/Icons/Extension.svg',
     ],
     'ffpitheme_participate' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:ffpi_theme/Resources/Public/Icons/Extension.svg',
     ],
 ];
